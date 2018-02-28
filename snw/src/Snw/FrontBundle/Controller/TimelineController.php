92d9ed6b07278a4ab4145dec1f3af7ce5f98fcd2
Omid Monavari <omid@Omid>
2018-02-28 10:19:54 +0100

    Modification des repository

--- snw/src/Snw/FrontBundle/Controller/TimelineController.php
+++ snw/src/Snw/FrontBundle/Controller/TimelineController.php
@@ -10,7 +10,8 @@ class TimelineController extends Controller
 	public function timelineAction()
 	{
 		$em = $this->getDoctrine()->getManager();
-		$statuses = $em->getRepository('FrontBundle:Status')->findAll();
+		$repository = $em->getRepository('FrontBundle:Status');
+		$statuses = $repository->findAll();
 
 		return $this->render('FrontBundle:Timeline:timeline.html.twig', array(
 			'statues' => $statuses
@@ -27,12 +28,28 @@ class TimelineController extends Controller
 			$this->createNotFoundException("Aucun utlisateur trouvé.");
 		}
 
-		$statuses = $em->getRepository('FrontBundle:Status')->findBy(array(
-			'user' => $user,
-			'deleted' => false
-		));
+		$statuses = $em->getRepository('FrontBundle:Status')->getUserTimeline($user)->getResult();
 
 		return $this->render('FrontBundle:Timeline:user_timeline.html.twig', array(
+			'user' => $user,
+			'statuses' => $statuses
+		));
+	}
+
+	public function friendsTimelineAction($userId)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$user = $em->getRepository('FrontBundle:User')->findOneById($userId);
+
+		if(!$user)
+		{
+			$this->createNotFoundException("L'utilisateur n'a pas été trouvé.");
+		}
+
+		$statuses = $em->getRepository('FrontBundle:Status')->getFriendsTimeline($user)->getResult();
+
+		return $this->render('FrontBundle:Timeline:friends_timeline.html.twig', array(
 			'user' => $user,
 			'statuses' => $statuses
 		));
